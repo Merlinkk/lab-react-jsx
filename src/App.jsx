dca1f6b208925d81155dfb830acec74a131f6e22
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-10 15:12:45 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import './App.css';
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
+const imageData = () => {
   let data = [
     {
-      id:1,
-      img:elephant
+      id: 1,
+      img: elephant,
     },
     {
-      id:2,
-      img:elephant
+      id: 2,
+      img: elephant,
     },
     {
-      id:3,
-      img:elephant
+      id: 3,
+      img: elephant,
     },
     {
-      id:4,
-      img:elephant
-    }
-  ]
+      id: 4,
+      img: elephant,
+    },
+  ];
   return data;
-}
+};
 
 function App() {
+  let imgData = imageData();
   // code here
+  return (
+    <>
+      <h1>Kalvium Gallary - App.jsx</h1>
+      <div className="container">
+        {imgData.map((e) => {
+          return <img key={e.id} src={e.img} alt="" />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,42 @@
 import { Component } from "react";
-import "./App.css"
+import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
+export default class AppClass extends Component {
+  imageData = () => {
     let data = [
       {
-        id:1,
-        img:elephant
+        id: 1,
+        img: elephant,
       },
       {
-        id:2,
-        img:elephant
+        id: 2,
+        img: elephant,
       },
       {
-        id:3,
-        img:elephant
+        id: 3,
+        img: elephant,
       },
       {
-        id:4,
-        img:elephant
-      }
-    ]
+        id: 4,
+        img: elephant,
+      },
+    ];
     return data;
-  }
+  };
 
   // code here
+  render() {
+    let imgData = this.imageData();
+    return (
+      <>
+        <h1>Kalvium Gallary - AppClass.jsx</h1>
+        <div className="container">
+          {imgData.map((e) => (
+            <img key={e.id} src={e.img} alt="" />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
