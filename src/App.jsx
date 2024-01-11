37ec2b34969608d5d236eb1e1bb4773db9e7c511
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-11 10:51:14 +0530

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import elephant from "./images/elephant.jpeg";
 
 const imageData = () => {
+  // differentiate by ID 
   let data = [
     {
       id: 1,
@@ -30,6 +31,7 @@ function App() {
     <>
       <h1>Kalvium Gallary - App.jsx</h1>
       <div className="container">
+        {/* use map to traverse throught the array */}
         {imgData.map((e) => {
           return <img key={e.id} src={e.img} alt="" />;
         })}
